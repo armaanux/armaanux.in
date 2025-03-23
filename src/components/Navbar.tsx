da49ff7c12a168f99252688487eba59e12ba427e
sidonweb <heysid88@gmail.com>
2025-03-24 00:27:37 +0530

    Adjust Navbar padding for improved layout consistency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,7 +41,7 @@ export const Navbar = ({ quote }: NavbarProps) => {
 
   return (
     <navbar id="navbar">
-      <section className="flex md:flex-row-reverse bg-transparent justify-between items-center px-4 md:px-14 py-10">
+      <section className="flex md:flex-row-reverse bg-transparent justify-between items-center px-7 md:px-14 py-10">
         {/* Left Side: Quote */}
         <h3 className="text-white text-sm md:text-base w-64">
           {quote}
